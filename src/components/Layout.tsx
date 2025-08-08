8dac17ea34cbe4971efab661323332a24cdd7956
Amybuild <tondyorfaq@gmail.com>
2025-08-08 22:56:47 +0700

    Reverted all changes back to version 70cceb36fac09a3110af3625845ce0ad0decbc83

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
-import { TaggingProvider } from "@/context/TaggingContext";
 
 const Layout = () => {
   return (
-    <TaggingProvider>
-      <div className="flex h-screen bg-background">
-        <Sidebar />
-        <div className="flex flex-col flex-1">
-          <Header />
-          <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
-            <Outlet />
-          </main>
-        </div>
+    <div className="flex h-screen bg-background">
+      <Sidebar />
+      <div className="flex flex-col flex-1">
+        <Header />
+        <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
+          <Outlet />
+        </main>
       </div>
-    </TaggingProvider>
+    </div>
   );
 };
 
